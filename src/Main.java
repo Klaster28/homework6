2182b537e8d0553473c6a59065f3b0325746390a
Klaster28 <89096914521@mail.ru>
2022-10-03 20:57:06 +0300

    Коммит после первого задания

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+     for (int i =1; i < 11; i++) {
+         System.out.println(i);
+     }
+
+
+     for (int a = 10; a > 0 ; a--) {
+         System.out.println(a);
+     }
+
+     for (int s = 0; s < 17; s = s+2) {
+         System.out.println(s);
+     }
+
+     for (int z = 10; z >-11; z--) {
+         System.out.println(z);
+     }
 
     }
 }
