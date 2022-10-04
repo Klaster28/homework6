d1e278b06f2c6fe2489044058b7e2ba6148c7d88
Klaster28 <89096914521@mail.ru>
2022-10-04 14:10:25 +0300

    Коммит после третьего задания

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,42 @@
 public class Main {
     public static void main(String[] args) {
+        for (int i =1; i < 11; i++) {
+            System.out.println(i);
+        }
 
-     for (int i =1; i < 11; i++) {
-         System.out.println(i);
-     }
+
+        for (int a = 10; a > 0 ; a--) {
+            System.out.println(a);
+        }
+
+        for (int s = 0; s < 17; s = s+2) {
+            System.out.println(s);
+        }
+        for (int z = 10; z >-11; z--) {
+            System.out.println(z);
+        }
+        for (int r = 1904; r < 2096; r = r+4) {
+            System.out.println(r+ "год является високосным!");
+        }
+
+        int contributionBob = 29000;
+        int total = 0;
+        for (int q = 1; q<13; q++) {
+            total = total + contributionBob ;
+            System.out.println("Меяц " + q + " сумма накопления равна " + total);
+        }
+
+        int contributionRob = 29000;
+        int totAl = 0;
+        for (int q = 1; q<13; q++) {
+            totAl = totAl + contributionRob ;
+            totAl = totAl + totAl/100;
+            System.out.println("Меяц " + q + " сумма накопления равна " + totAl);
+        }
 
 
-     for (int a = 10; a > 0 ; a--) {
-         System.out.println(a);
-     }
 
-     for (int s = 0; s < 17; s = s+2) {
-         System.out.println(s);
-     }
 
-     for (int z = 10; z >-11; z--) {
-         System.out.println(z);
-     }
-
-     for (int r = 1904; r < 2096; r = r+4) {
-         System.out.println(r+ "год является високосным!");
-     }
-
-     for (int t = 7; t < 100; t = t+7) {
-         System.out.println(t);
-     }
-
-     for (int d = 1; d < 515; d = d*2) {
-         System.out.println(d);
-     }
 
 
     }
