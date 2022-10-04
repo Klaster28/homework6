51a1ad974b92528c67d7726cbac35d117400668f
Klaster28 <89096914521@mail.ru>
2022-10-04 10:36:06 +0300

    Коммит после второго задания

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,18 @@ public class Main {
          System.out.println(z);
      }
 
+     for (int r = 1904; r < 2096; r = r+4) {
+         System.out.println(r+ "год является високосным!");
+     }
+
+     for (int t = 7; t < 100; t = t+7) {
+         System.out.println(t);
+     }
+
+     for (int d = 1; d < 515; d = d*2) {
+         System.out.println(d);
+     }
+
+
     }
 }
